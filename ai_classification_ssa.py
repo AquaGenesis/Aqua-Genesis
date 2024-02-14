b834a1549dde6c14dba7b2b81053b7cf34f72270
Alanwar <alanwarlimdi@gmail.com>
2024-02-14 23:33:36 +0400

    third

--- ai_classification_ssa.py
+++ ai_classification_ssa.py
@@ -26,7 +26,7 @@ prediction = model.predict(new_data)
 print(prediction)
 
 if prediction[0] <= 0.5:    
-    diagnosis = 'High carbon content detected - release NPK'
+    diagnosis = 'High carbon content detected - release NPKN'
 else:
     diagnosis = "NPK not required at the present moment"
 
